c762b0a30eddddd73f751acf65beb3222882d9a8
AbelCheng <abelcys@gmail.com>
2015-02-02 14:14:50 -0500

    example AssemblyDescription

--- Examples/MyDbWebApi/Properties/AssemblyInfo.cs
+++ Examples/MyDbWebApi/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MyDbWebApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An example of using DataBooster.DbWebApi. https://github.com/DataBooster/DbWebApi")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DataBooster")]
 [assembly: AssemblyProduct("MyDbWebApi")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
