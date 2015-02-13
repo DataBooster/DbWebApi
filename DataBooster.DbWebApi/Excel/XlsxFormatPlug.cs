cb93cb7e464b12963600684a4d9c55c3c67a8b42
AbelCheng <abelcys@gmail.com>
2015-02-12 23:03:22 -0500

    Adding Excel support

--- DataBooster.DbWebApi/Excel/XlsxFormatPlug.cs
+++ DataBooster.DbWebApi/Excel/XlsxFormatPlug.cs
@@ -1,11 +1,13 @@
 ﻿// Copyright (c) 2015 Abel Cheng <abelcys@gmail.com> and other contributors.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.IO;
 using System.Text;
 using System.Web.Http;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Collections.Generic;
+using ClosedXML.Excel;
 
 namespace DataBooster.DbWebApi.Excel
 {
@@ -40,7 +42,34 @@ namespace DataBooster.DbWebApi.Excel
 			MediaTypeHeaderValue negotiatedMediaType, Encoding negotiatedEncoding)
 		{
 			HttpResponseMessage csvResponse = apiController.Request.CreateResponse();
+			XLWorkbook workbook = new XLWorkbook();
+			IXLWorksheet currentWorksheet = null;
 
+			using (DbContext dbContext = new DbContext())
+			{
+				dbContext.ExecuteDbApi(sp, parameters, false, rs =>
+					{
+						currentWorksheet = workbook.AddWorksheet(string.Format("Sheet{0}", rs));
+					},
+					header =>
+					{
+						if (currentWorksheet != null)
+							for (int col = 0; col < header.VisibleFieldCount; col++)
+								currentWorksheet.Cell(1, col + 1).SetValue(header.GetName(col));
+					},
+					rows =>
+					{
+						if (currentWorksheet != null)
+							currentWorksheet.Cell(2, 1).SetValue(rows);
+					},
+					null, null, true);
+			}
+
+			MemoryStream memoryStream = new MemoryStream();
+			workbook.SaveAs(memoryStream);
+			memoryStream.Seek(0, SeekOrigin.Begin);
+			csvResponse.Content = new StreamContent(memoryStream);
+			csvResponse.Content.Headers.ContentLength = memoryStream.Length;
 
 			csvResponse.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment") { FileName = "[save_as]." + FormatShortName };
 
