03529ac8ce20106ab04bbfc9e6afb154060ff58f
AbelCheng <abelcys@gmail.com>
2015-03-03 22:39:09 -0500

    Preparing for client library

---
+++ Client/Net40/DataBooster.DbWebApi.Client/DbWebApiResponse.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2015 Abel Cheng <abelcys@gmail.com> and other contributors.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Newtonsoft.Json.Linq;
+
+namespace DataBooster.DbWebApi.Client
+{
+	public class DbWebApiResponse
+	{
+		public JObject[][] ResultSets { get; set; }
+		public JObject OutputParameters { get; set; }
+		public object ReturnValue { get; set; }
+	}
+}
---
+++ Client/Net40/DataBooster.DbWebApi.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataBooster.DbWebApi.Client")]
+[assembly: AssemblyDescription("DbWebApi .Net 4.0 Client Library. https://github.com/DataBooster/DbWebApi")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Abel Cheng")]
+[assembly: AssemblyProduct("DataBooster.DbWebApi.Client")]
+[assembly: AssemblyCopyright("Copyright © Abel Cheng 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("097a7060-2364-4c52-a416-9c5f6288ff6b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyInformationalVersion("1.0.1-alpha")]
