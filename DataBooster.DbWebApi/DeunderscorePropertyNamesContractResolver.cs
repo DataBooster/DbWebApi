8aa536df8ed27af5ae4dcc96c1f2781076ab5a17
AbelCheng <abelcys@gmail.com>
2015-03-09 12:00:25 -0400

    Added DeunderscorePropertyNamesContractResolver.cs

--- DataBooster.DbWebApi/DeunderscorePropertyNamesContractResolver.cs
+++ DataBooster.DbWebApi/DeunderscorePropertyNamesContractResolver.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Globalization;
 using Newtonsoft.Json.Serialization;
 
 namespace DataBooster.DbWebApi
@@ -11,21 +8,59 @@ namespace DataBooster.DbWebApi
 	/// </summary>
 	public class DeunderscorePropertyNamesContractResolver : DefaultContractResolver
 	{
-		public DeunderscorePropertyNamesContractResolver()
+		private bool _CamelCase;
+
+		public DeunderscorePropertyNamesContractResolver(bool camelCase = false)
 			: base(true)
 		{
+			_CamelCase = camelCase;
 		}
 
 		/// <param name="propertyName">Name of the property.</param>
 		/// <returns>Name of the property.</returns>
 		protected override string ResolvePropertyName(string propertyName)
 		{
-			if (string.IsNullOrEmpty(propertyName))
+			if (string.IsNullOrEmpty(propertyName) || propertyName.Length == 1)
 				return propertyName;
 
-			int cntUpper = 0, cntLower = 0;
+			char[] pascalChars = new char[propertyName.Length];
+			int cntUpper = 0, cntLower = 0, lenFragment = 0, lenPascal = 0;
 
-			return propertyName;
+			foreach (char c in propertyName)
+			{
+				if (char.IsUpper(c))
+				{
+					pascalChars[lenPascal] = (lenFragment == 0) ? c : char.ToLower(c, CultureInfo.InvariantCulture);
+					lenPascal++;
+					lenFragment++;
+					cntUpper++;
+				}
+				else if (char.IsLower(c))
+				{
+					pascalChars[lenPascal] = (lenFragment == 0) ? char.ToUpper(c, CultureInfo.InvariantCulture) : c;
+					lenPascal++;
+					lenFragment++;
+					cntLower++;
+				}
+				else if (char.IsPunctuation(c) || char.IsWhiteSpace(c))
+				{
+					lenFragment = 0;
+				}
+				else
+				{
+					pascalChars[lenPascal] = c;
+					lenPascal++;
+					lenFragment = 0;
+				}
+			}
+
+			if (lenPascal == 0)
+				return string.Empty;
+			else
+				if (_CamelCase && char.IsUpper(pascalChars[0]))
+					pascalChars[0] = char.ToLower(pascalChars[0], CultureInfo.InvariantCulture);
+
+			return (cntUpper > 0 && cntLower > 0) ? propertyName : new string(pascalChars, 0, lenPascal);
 		}
 	}
 }
