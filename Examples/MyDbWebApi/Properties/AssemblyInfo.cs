6e3e019e1d3bb34d1b02a625cc18afa52dd2117b
AbelCheng <abelcys@gmail.com>
2015-04-01 14:38:11 -0400

    Updated example server MyDbWebApi to 1.2.5.1

--- Examples/MyDbWebApi/Properties/AssemblyInfo.cs
+++ Examples/MyDbWebApi/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MyDbWebApi")]
-[assembly: AssemblyDescription("An example of using DataBooster.DbWebApi. https://github.com/DataBooster/DbWebApi")]
+[assembly: AssemblyDescription("An example ASP.NET Web API Server of DataBooster.DbWebApi. https://github.com/DataBooster/DbWebApi")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DataBooster")]
 [assembly: AssemblyProduct("MyDbWebApi")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.2.5.1")]
+[assembly: AssemblyFileVersion("1.2.5.1")]
