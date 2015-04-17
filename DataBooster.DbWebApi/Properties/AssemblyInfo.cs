04c8bd06a197bfc59ad75731e5dfaebd75c0f1bc
AbelCheng <abelcys@gmail.com>
2015-04-17 14:16:49 -0400

    Updated nuspec files to include sql scripts

--- DataBooster.DbWebApi/Properties/AssemblyInfo.cs
+++ DataBooster.DbWebApi/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.5.5")]
 [assembly: AssemblyFileVersion("1.2.5.5")]
-[assembly: AssemblyInformationalVersion("1.2.5.5-beta")]
+[assembly: AssemblyInformationalVersion("1.2.5.5")]
