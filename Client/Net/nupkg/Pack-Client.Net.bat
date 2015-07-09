9c884fc0f145ef8de6b19d51137f93ae2f04d969
Abel Cheng <abelcys@gmail.com>
2015-07-09 00:55:03 -0400

    Preparing for net45 packages ...

--- Client/Net/nupkg/Pack-Client.Net.bat
+++ Client/Net/nupkg/Pack-Client.Net.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 CD /D %~dp0
-..\..\..\.nuget\NuGet.exe pack ..\DataBooster.DbWebApi.Client\DataBooster.DbWebApi.Client.csproj -IncludeReferencedProjects -Prop Configuration=Release;Platform=AnyCPU
+..\..\..\.nuget\NuGet.exe pack ..\DataBooster.DbWebApi.Client\DataBooster.DbWebApi.Client.csproj -IncludeReferencedProjects -Symbols -Prop Configuration=Release;Platform=AnyCPU
