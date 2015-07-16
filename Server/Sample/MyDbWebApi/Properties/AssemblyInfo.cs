40092dfca94bcd30918bcd710bcc57afffb3b8e6
Abel Cheng <abelcys@gmail.com>
2015-07-16 00:28:28 -0400

    Preparing for net45 sample

---
+++ Server/Sample/MyDbWebApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyDbWebApi")]
+[assembly: AssemblyDescription("An example ASP.NET Web API Server of DataBooster.DbWebApi. https://github.com/DataBooster/DbWebApi")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DataBooster")]
+[assembly: AssemblyProduct("MyDbWebApi")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a60202d7-1802-4a17-9160-65ffa89ff199")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
