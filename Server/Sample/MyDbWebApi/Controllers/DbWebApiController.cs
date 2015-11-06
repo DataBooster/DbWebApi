d502f3d778eb0ab72b61b48b65cd0f07f53c5def
Abel Cheng <abelcys@gmail.com>
2015-11-06 00:59:59 -0500

    Updated example servers to 1.2.8-beta

--- Server/Sample/MyDbWebApi/Controllers/DbWebApiController.cs
+++ Server/Sample/MyDbWebApi/Controllers/DbWebApiController.cs
@@ -1,80 +1,28 @@
-﻿using System.Net;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Web.Http;
-using System.Collections.Generic;
 using DataBooster.DbWebApi;
-using Newtonsoft.Json.Linq;
 
 namespace MyDbWebApi.Controllers
 {
 	[DbWebApiAuthorize]
 	public class DbWebApiController : ApiController
 	{
-		#region Approach 1: Auto-detect a post request body. Invoking BulkExecute if sets of input parameters are wrapped in an arrray; or invoking Execute if input parameters are wrapped in a single dictionary.
-
+		#region Auto-detect a post request body. Invoking BulkExecute if sets of input parameters are wrapped in an arrray; or invoking Execute if input parameters are wrapped in a single dictionary.
 		[AcceptVerbs("GET", "POST", "PUT", "DELETE", "OPTIONS")]
-		public HttpResponseMessage DynExecute(string sp, JContainer requestBody)
+		public HttpResponseMessage DynExecute(string sp, InputParameters dynParameters)
 		{
-			if (requestBody == null)
-				return Execute(sp, null);
-
-			JObject parameters = requestBody as JObject;
-
-			if (parameters != null)
-				return Execute(sp, parameters.ToObject<Dictionary<string, object>>());
-
-			JArray bulkParameters = requestBody as JArray;
-
-			if (bulkParameters != null)
-			{
-				List<Dictionary<string, object>> listOfDicts = bulkParameters.ToObject<List<Dictionary<string, object>>>();
-
-				if (listOfDicts != null && listOfDicts.Count > 0)
-					return BulkExecute(sp, listOfDicts);
-				else
-					return Request.CreateResponse(HttpStatusCode.NoContent);
-			}
-
-			return Request.CreateResponse(HttpStatusCode.BadRequest);
-		}
-
-		#endregion	// Approach 1
-
-		#region Approach 2: Separate BulkExecute action from Execute action
-
-		[AcceptVerbs("GET", "POST", "PUT", "DELETE", "OPTIONS")]
-		public HttpResponseMessage Execute(string sp, Dictionary<string, object> parameters)
-		{
-			IDictionary<string, object> inputDict = Request.GatherInputParameters(parameters);
+			dynParameters.SupplementQueryString(Request);
 
 			if (!string.IsNullOrEmpty(ConfigHelper.UserNameReservedParameter) && User != null && User.Identity != null)
 			{
 				string userName = User.Identity.Name;
 
 				if (!string.IsNullOrEmpty(userName))
-					inputDict[ConfigHelper.UserNameReservedParameter] = userName;
+					dynParameters.SetParameter(ConfigHelper.UserNameReservedParameter, userName);
 			}
 
-			return this.ExecuteDbApi(sp, inputDict);
+			return this.DynExecuteDbApi(sp, dynParameters);
 		}
-
-		[AcceptVerbs("POST", "PUT")]
-		public HttpResponseMessage BulkExecute(string sp, List<Dictionary<string, object>> listOfParametersDict)
-		{
-			Request.BulkGatherInputParameters(listOfParametersDict);
-
-			if (!string.IsNullOrEmpty(ConfigHelper.UserNameReservedParameter) && User != null && User.Identity != null)
-			{
-				string userName = User.Identity.Name;
-
-				if (!string.IsNullOrEmpty(userName))
-					foreach (var inputDict in listOfParametersDict)
-						inputDict[ConfigHelper.UserNameReservedParameter] = userName;
-			}
-
-			return this.BulkExecuteDbApi(sp, listOfParametersDict);
-		}
-
-		#endregion	// Approach 2
+		#endregion
 	}
 }
