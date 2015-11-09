dcdfc37353084584fb247986745eec7683e86317
Abel Cheng <abelcys@gmail.com>
2015-11-09 10:16:57 -0500

    Updated DbWebApiController

--- Server/Sample/MyDbWebApi/Controllers/DbWebApiController.cs
+++ Server/Sample/MyDbWebApi/Controllers/DbWebApiController.cs
@@ -7,15 +7,27 @@ namespace MyDbWebApi.Controllers
 	[DbWebApiAuthorize]
 	public class DbWebApiController : ApiController
 	{
-		// Auto-detect a post request body. Invoking BulkExecute if sets of input parameters are wrapped in an arrray; or invoking Execute if input parameters are wrapped in a single dictionary.
+		/// <summary>
+		/// Auto-detect a post request body. Invoking BulkExecute if sets of input parameters are wrapped in an arrray; or invoking Execute if input parameters are wrapped in a single dictionary.
+		/// </summary>
+		/// <param name="sp">Stored Procedure's fully qualified name</param>
+		/// <param name="dynParameters">Auto-binding from the request body</param>
+		/// <returns></returns>
 		[AcceptVerbs("GET", "POST", "PUT", "DELETE", "OPTIONS")]
 		public HttpResponseMessage DynExecute(string sp, InputParameters dynParameters)
 		{
 			if (dynParameters == null)
-				dynParameters = new InputParameters(Request);
+				dynParameters = new InputParameters(Request);	// Gather input parameters from the URI (query string) if not in the request body.
 			else
-				dynParameters.SupplementQueryString(Request);
+				dynParameters.SupplementQueryString(Request);	// Accept URI query string as as a supplementary.
 
+			SetUserName(dynParameters);							// Set the conventional User Name Parameter if configured.
+
+			return this.DynExecuteDbApi(sp, dynParameters);		// The main entry point to call the DbWebApi.
+		}
+
+		private void SetUserName(InputParameters dynParameters)
+		{
 			if (!string.IsNullOrEmpty(ConfigHelper.UserNameReservedParameter) && User != null && User.Identity != null)
 			{
 				string userName = User.Identity.Name;
@@ -23,8 +35,6 @@ namespace MyDbWebApi.Controllers
 				if (!string.IsNullOrEmpty(userName))
 					dynParameters.SetParameter(ConfigHelper.UserNameReservedParameter, userName);
 			}
-
-			return this.DynExecuteDbApi(sp, dynParameters);
 		}
 	}
 }
