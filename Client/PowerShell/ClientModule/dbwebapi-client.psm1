d2f0c8468014cb8e27dae53bd9ccb33efad46fc9
AbelCheng <abelcys@gmail.com>
2016-01-14 17:13:28 -0500

    Adding Client PowerShell Module Project

---
+++ Client/PowerShell/ClientModule/dbwebapi-client.psm1
@@ -0,0 +1,59 @@
+<#
+	Invoke-DbWebApi
+
+#>
+
+Function Invoke-DbWebApi {
+	[CmdletBinding(DefaultParameterSetName = 'DefaultAuth', SupportsShouldProcess)]
+	Param (
+		[Parameter(Mandatory)]
+		[Uri]$Uri,
+		[Parameter()]
+		[Microsoft.PowerShell.Commands.WebRequestMethod]$Method = [Microsoft.PowerShell.Commands.WebRequestMethod]::Post,
+		[Parameter(ValueFromPipeline)]
+		$Body,
+		[Parameter()]
+		[string]$ContentType = "application/json",
+		[Parameter()]
+		[string]$InFile,
+		[Parameter()]
+		[string]$OutFile,
+		[Parameter(Mandatory, ParameterSetName = 'BasicAuth')]
+		[ValidateNotNullOrEmpty()]
+		[string]$User,
+		[Parameter(Mandatory, ParameterSetName = 'BasicAuth')]
+		[string]$Password
+	)
+
+	begin
+	{
+		if ($PSCmdlet.ParameterSetName -eq 'BasicAuth') {
+			$basicAuth = "Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($($User) + ":" + $($Password)));
+			$psBoundParameters.Headers = @{ Authorization = $basicAuth };
+			$psBoundParameters.Remove("User");
+			$psBoundParameters.Remove("Password");
+		}
+		else {
+			$psBoundParameters.UseDefaultCredentials = $true;
+		}
+
+		if (!$psBoundParameters.ContainsKey("Method")) {
+			$psBoundParameters.Method = $Method;
+		}
+
+		if (!$psBoundParameters.ContainsKey("ContentType")) {
+			$psBoundParameters.ContentType = $ContentType;
+		}
+	}
+
+	process
+	{
+		if ($Body -and $ContentType -eq "application/json" -and $Body -isnot [string]) {
+			$psBoundParameters.Body = ConvertTo-Json $Body;
+		}
+
+		if ($PSCmdlet.ShouldProcess($psBoundParameters)) {
+			Invoke-RestMethod @psBoundParameters;
+		}
+	}
+}
