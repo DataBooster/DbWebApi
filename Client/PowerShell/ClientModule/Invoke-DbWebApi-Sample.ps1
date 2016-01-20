bddc7d4ac83b7fb606ff934b861f6771eea131b9
AbelCheng <abelcys@gmail.com>
2016-01-20 14:22:50 -0500

    Updated Invoke-DbWebApi-Sample.ps1

--- Client/PowerShell/ClientModule/Invoke-DbWebApi-Sample.ps1
+++ Client/PowerShell/ClientModule/Invoke-DbWebApi-Sample.ps1
@@ -2,9 +2,11 @@
 # https://github.com/databooster/dbwebapi
 # Date: 2015-06-09
 
-Import-Module Microsoft.PowerShell.Utility
+Import-Module "Microsoft.PowerShell.Utility"
 Import-Module "$PSScriptRoot\DbWebApi-Client.psm1"
 
 Invoke-DbWebApi @args
 
-Remove-Module DbWebApi-Client
+# Remove-Module DbWebApi-Client
+
+Exit $error.Count;
--- Client/PowerShell/ClientModule/Invoke-DbWebApi.bat
+++ Client/PowerShell/ClientModule/Invoke-DbWebApi.bat
@@ -3,4 +3,6 @@ REM Invoke-DbWebApi.bat - DbWebApi Client PowerShell Utility
 REM https://github.com/databooster/dbwebapi
 REM Date: 2016-01-20
 
-PowerShell.exe -ExecutionPolicy Bypass -NonInteractive -File "%~dp0\Invoke-DbWebApi-Sample.ps1" %*
+PowerShell.exe -ExecutionPolicy Bypass -File "%~dp0\Invoke-DbWebApi-Sample.ps1" %*
+
+REM EXIT /B %ERRORLEVEL%
