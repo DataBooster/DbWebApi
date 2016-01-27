1058c442744196235f05eaad87623cbeef1233d7
AbelCheng <abelcys@gmail.com>
2016-01-27 15:30:47 -0500

    Updated DbWebApi-Client.psm1

--- Client/PowerShell/ClientModule/DbWebApi-Client.psm1
+++ Client/PowerShell/ClientModule/DbWebApi-Client.psm1
@@ -15,34 +15,106 @@
 	.INPUTS
 	System.Object. You can pipe the body of a web request to Invoke-DbWebApi. The $Body will be passed in as-is to Invoke-RestMethod if it is a string, otherwise it will be treated as a object and converted to JSON string before passing to Invoke-RestMethod if ContentType is "application/json".
 
-	.PARAMETER Body
-	Enhanced: The Body value will be passed in as-is to Invoke-RestMethod if it is a string, otherwise it will be treated as a object and converted to JSON string before passing to Invoke-RestMethod if ContentType is "application/json".
-
 	.PARAMETER Uri
 	Specifies the Uniform Resource Identifier (URI) of the Internet resource to which the web request is sent. This parameter supports HTTP, HTTPS, FTP, and FILE values.
+
+	(Inherited from Invoke-RestMethod)
 
 	.PARAMETER Method
 	Specifies the method used for the web api request. Defaults to Post.
 
+	(Inherited from Invoke-RestMethod)
+
+	.PARAMETER Body
+	Enhanced: The Body value will be passed in as-is to Invoke-RestMethod if it is a string, otherwise it will be treated as a object and converted to JSON string before passing to Invoke-RestMethod if ContentType is "application/json".
+
+	(Inherited from Invoke-RestMethod)
+
 	.PARAMETER ContentType
 	Specifies the content type of the web request. If this parameter is omitted, Invoke-DbWebApi sets the content type to "application/json".
+
+	(Inherited from Invoke-RestMethod)
 
 	.PARAMETER InFile
 	Gets the content of the web request from a file.
 
 	Enter a path and file name. If you omit the path, the default is the current location.
 
+	(Inherited from Invoke-RestMethod)
+
 	.PARAMETER OutFile
 	Saves the response body in the specified output file. Enter a path and file name. If you omit the path, the default is the current location.
 
 	By default, Invoke-DbWebApi returns the results to the pipeline. To send the results to a file and to the pipeline, use the Passthru parameter.
 
-	.PARAMETER User
-	Specifies a user name if you want to use Basic Authorization to send the request.
-	Otherwise Invoke-DbWebApi always uses the credentials of the current user to send the web request. -UseDefaultCredentials
+	(Inherited from Invoke-RestMethod)
 
-	.PARAMETER Password
-	Specifies the password of the specified user name, once you use Basic Authorization to send the request.
+	.PARAMETER PassThru
+	Returns the results, in addition to writing them to a file. This parameter is valid only when the OutFile parameter is also used in the command.
+
+	(Inherited from Invoke-RestMethod)
+
+	.PARAMETER Credential
+	Specifies a user account that has permission to send the request.
+
+	(Inherited from Invoke-RestMethod)
+
+	.PARAMETER BasicAuth
+	Force to send the Basic Authorization header on the initial request if the Credential is presented. The plaintext username and password will be extracted from Credential parameter (PSCredential class).
+
+	By default, Invoke-RestMethod will not send the Authorization header on the initial request. It waits for a challenge response then re-sends the request with the Authorization header.
+
+	.PARAMETER Headers
+	Specifies the headers of the web request. Enter a hash table or dictionary.
+
+	(Inherited from Invoke-RestMethod)
+
+	.PARAMETER Certificate
+	Specifies the client certificate that is used for a secure web request. Enter a variable that contains a certificate or a command or expression that gets the certificate.
+
+	(Inherited from Invoke-RestMethod)
+
+	.PARAMETER CertificateThumbprint
+	Specifies the digital public key certificate (X509) of a user account that has permission to send the request. Enter the certificate thumbprint of the certificate.
+
+	Certificates are used in client certificate-based authentication. They can be mapped only to local user accounts; they do not work with domain accounts.
+
+	(Inherited from Invoke-RestMethod)
+
+	.PARAMETER DisableKeepAlive
+	Sets the KeepAlive value in the HTTP header to False. By default, KeepAlive is True. KeepAlive establishes a persistent connection to the server to facilitate subsequent requests.
+
+	(Inherited from Invoke-RestMethod)
+
+	.PARAMETER TimeoutSec
+	Specifies how long the request can be pending before it times out. Enter a value in seconds. The default value, 0, specifies an indefinite time-out.
+
+	(Inherited from Invoke-RestMethod)
+
+	.PARAMETER MaximumRedirection
+	Determines how many times Windows PowerShell redirects a connection to an alternate Uniform Resource Identifier (URI) before the connection fails. The default value is 5. A value of 0 (zero) prevents all redirection.
+
+	(Inherited from Invoke-RestMethod)
+
+	.PARAMETER SessionVariable
+	Creates a web request session and saves it in the value of the specified variable. Enter a variable name without the dollar sign ($) symbol. You cannot use the SessionVariable and WebSession parameters in the same command.
+
+	(Inherited from Invoke-RestMethod)
+
+	.PARAMETER TransferEncoding
+	Specifies a value for the transfer-encoding HTTP response header. Valid values are Chunked, Compress, Deflate, GZip and Identity.
+
+	(Inherited from Invoke-RestMethod)
+
+	.PARAMETER UserAgent
+	Specifies a user agent string for the web request.
+
+	(Inherited from Invoke-RestMethod)
+
+	.PARAMETER WebSession
+	Specifies a web request session. Enter the variable name, including the dollar sign ($). You cannot use the SessionVariable and WebSession parameters in the same command.
+
+	(Inherited from Invoke-RestMethod)
 
 	.NOTES
 	Since the Invoke-DbWebApi is just a simple wrap of the build-in Invoke-RestMethod cmdlet.
@@ -50,11 +122,6 @@
 
 	.EXAMPLE
 	$Response = Invoke-DbWebApi -Uri https://test-server:8089/something -Body @{inParamA = [DateTime]"2015-03-16"; inParamB = 108; inParamC = "Hello"}
-	(Using Integrated Windows Authentication.)
-
-	.EXAMPLE
-	Invoke-DbWebApi -Uri https://test-server:8089/somethingelse/csv -Method Get -User guest108 -Password "Hello World!" -OutFile "C:\Temp\test.csv"
-	(Using Basic Authentication.)
 
 	.LINK
 	https://github.com/DataBooster/DbWebApi
@@ -64,7 +131,7 @@
 #>
 
 Function Invoke-DbWebApi {
-	[CmdletBinding(DefaultParameterSetName = 'DefaultAuth', SupportsShouldProcess)]
+	[CmdletBinding(SupportsShouldProcess)]
 	Param (
 		[Parameter(Mandatory)]
 		[Uri]$Uri,
@@ -78,11 +145,13 @@ Function Invoke-DbWebApi {
 		[string]$InFile,
 		[Parameter()]
 		[string]$OutFile,
-		[Parameter(Mandatory, ParameterSetName = 'BasicAuth')]
-		[ValidateNotNullOrEmpty()]
-		[string]$User,
-		[Parameter(Mandatory, ParameterSetName = 'BasicAuth')]
-		[string]$Password,
+		[Parameter()]
+		[switch]$PassThru,
+
+		[Parameter()]
+		[PSCredential]$Credential,
+		[Parameter()]
+		[switch]$BasicAuth,
 
 		[Parameter()]
 		[System.Collections.IDictionary]$Headers,
@@ -91,17 +160,13 @@ Function Invoke-DbWebApi {
 		[Parameter()]
 		[string]$CertificateThumbprint,
 		[Parameter()]
-		[PSCredential]$Credential,
-		[Parameter()]
 		[switch]$DisableKeepAlive,
+		[Parameter()]
+		[int]$TimeoutSec,
 		[Parameter()]
 		[int]$MaximumRedirection,
 		[Parameter()]
-		[switch]$PassThru,
-		[Parameter()]
 		[string]$SessionVariable,
-		[Parameter()]
-		[int]$TimeoutSec,
 		[Parameter()]
 		[string]$TransferEncoding,
 		[Parameter()]
@@ -112,16 +177,18 @@ Function Invoke-DbWebApi {
 
 	Begin
 	{
-		if ($PSCmdlet.ParameterSetName -eq 'BasicAuth') {
-			$basicAuth = "Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($($User) + ":" + $($Password)));
-			if ($Headers) {
-				$psBoundParameters.Headers["Authorization"] = $basicAuth;
+		if ($Credential) {
+			if ($BasicAuth) {
+				$basicAuthHeader = "Basic " + [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($($Credential.UserName) + ":" + $($Credential.GetNetworkCredential().Password)));
+				if ($Headers) {
+					$psBoundParameters.Headers["Authorization"] = $basicAuthHeader;
+				}
+				else {
+					$psBoundParameters.Headers = @{ Authorization = $basicAuthHeader };
+				}
+				$psBoundParameters.Remove("BasicAuth") | Out-Null;
+				$psBoundParameters.Remove("Credential") | Out-Null;
 			}
-			else {
-				$psBoundParameters.Headers = @{ Authorization = $basicAuth };
-			}
-			$psBoundParameters.Remove("User") | Out-Null;
-			$psBoundParameters.Remove("Password") | Out-Null;
 		}
 		else {
 			$psBoundParameters.UseDefaultCredentials = $true;
