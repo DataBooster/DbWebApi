3da5c9a0326d9f504602f4b8b195b7d3ad6215fc
Abel Cheng <abelcys@gmail.com>
2017-06-12 20:30:11 -0400

    Fixed NullReferenceException bug in Client.CmdUtility.AppConfiguration.StrAcceptType

--- Client/CommandLine/WebApi-Client/AppConfiguration.cs
+++ Client/CommandLine/WebApi-Client/AppConfiguration.cs
@@ -12,28 +12,28 @@ namespace DataBooster.DbWebApi.Client.CmdUtility
 {
 	class AppConfiguration
 	{
-		[Option('u', "Uri", Required = true, HelpText = "Specifies the Uniform Resource Identifier (URI) of the Internet resource to which the web request is sent.")]
+		[Option('U', "Uri", Required = true, HelpText = "Specifies the Uniform Resource Identifier (URI) of the Internet resource to which the web request is sent.")]
 		public string Uri { get; set; }
 
-		[Option('m', "Method", DefaultValue = "Post", HelpText = "Specifies the method used for the web api request.")]
-		public string StrMethod { get { return Method.Method; } set { Method = new HttpMethod(value); } }
+		[Option('M', "Method", DefaultValue = "Post", HelpText = "Specifies the method used for the web api request.")]
+		public string StrMethod { get { return (Method == null) ? null : Method.Method; } set { Method = new HttpMethod(value); } }
 		public HttpMethod Method { get; private set; }
 
-		[Option('c', "ContentType", DefaultValue = "application/json", HelpText = "Specifies the content type of the web request.")]
+		[Option('C', "ContentType", DefaultValue = "application/json", HelpText = "Specifies the content type of the web request.")]
 		public string ContentType { get; set; }
 
-		[Option('a', "AcceptType", HelpText = "Specifies the expected media type of the response.")]
-		public string StrAcceptType { get { return AcceptType.MediaType; } set { AcceptType = new MediaTypeWithQualityHeaderValue(value); } }
+		[Option('A', "AcceptType", HelpText = "Specifies the expected media type of the response.")]
+		public string StrAcceptType { get { return (AcceptType == null) ? null : AcceptType.MediaType; } set { AcceptType = new MediaTypeWithQualityHeaderValue(value); } }
 		public MediaTypeWithQualityHeaderValue AcceptType { get; private set; }
 
-		[Option('t', "TimeoutSec", DefaultValue = 0, HelpText = "Specifies how long the request can be pending before it times out. Enter a value in seconds.")]
+		[Option('T', "TimeoutSec", DefaultValue = 0, HelpText = "Specifies how long the request can be pending before it times out. Enter a value in seconds.")]
 		public int TimeoutSec { get { return (int)Timeout.TotalSeconds; } set { Timeout = TimeSpan.FromSeconds(value); } }
 		public TimeSpan Timeout { get; private set; }
 
-		[Option('n', "userName", HelpText = "Specifies the username if using Basic Authorization for the web request.")]
+		[Option('N', "userName", HelpText = "Specifies the username if using Basic Authorization for the web request.")]
 		public string UserName { get; set; }
 
-		[Option('p', "Password", HelpText = "Specifies the password if using Basic Authorization for the web request.")]
+		[Option('P', "Password", HelpText = "Specifies the password if using Basic Authorization for the web request.")]
 		public string Password { get; set; }
 
 		[ParserState]
