ac1e036aa621ca182657b03327ce865c715e60d6
Abel Cheng <abelcys@gmail.com>
2017-12-06 00:15:22 -0500

    Updated sample servers to 1.2.12.2

--- Server/Sample/DbWebApi.OwinSample/Properties/AssemblyInfo.cs
+++ Server/Sample/DbWebApi.OwinSample/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.12.1")]
-[assembly: AssemblyFileVersion("1.2.12.1")]
+[assembly: AssemblyVersion("1.2.12.2")]
+[assembly: AssemblyFileVersion("1.2.12.2")]
